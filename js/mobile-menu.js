1b337ef326e3dc8c09647d268ee03ebba815d461
Genius Space HTML/CSS <124382088+genius-space@users.noreply.github.com>
2023-04-06 09:32:24 +0300

    Add files via upload

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,11 @@ const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const disableScroll = () =>
+  document.body.classList.toggle('is-scroll-disabled');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+menuBtnOpen.addEventListener('click', disableScroll);
+menuBtnClose.addEventListener('click', disableScroll);
